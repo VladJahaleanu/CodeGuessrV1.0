f71c0a98ed2f1251d9aa17b87288c00bda0962eb
VladJahaleanu <vlad.jahaleanu01@gmail.com>
2021-05-16 04:24:38 +0300

    CodeGuessr v1.0.0 with a working backend

---
+++ frontend/script.js
@@ -0,0 +1,277 @@
+const body = document.getElementById('add_button_modal');
+
+const formLanguage = document.getElementById('formLanguage');
+const formImgUrl = document.getElementById('formImgUrl');
+
+let saveButton = document.getElementById('save');
+const cancelButton = document.getElementById('cancel');
+
+let addButton = document.createElement('button');
+// Add event on add button
+addButton.addEventListener('click', openAddModal);
+addButton.textContent = " + Add a code snippet";
+body.appendChild(addButton);
+addButton.classList.add("animatedButton");
+addButton.classList.add("add_button");
+
+var data1;
+var languages = ["C++", "Python", "Objective-C", "JavaScript", "Mindfuck", "Lua", "Swift", "Java", "SQL", "R", "Shell", "PHP", "Dart", "HTML"];
+var correctLanguage;
+var points = 0;
+var rightNumberIndex;
+
+const codeSnippet = document.getElementById("code_snippet");
+const answers = document.getElementById("answers");
+const next = document.getElementById("bottom");
+const retry = document.getElementById("final_score");
+
+fetch('http://localhost:3000/gists')
+  .then(
+    function (response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
+
+      // Examine the text in the response
+      response.json().then(function (data) {
+        console.log(data);
+        data1 = data;
+        renderGists(data);
+      });
+    }
+  )
+  .catch(function (err) {
+    console.log('Fetch Error :-S', err);
+  });
+
+function renderGists (gists) {
+
+
+refresh();
+}
+
+// Add article
+function addGistToServer() {
+    // creat post object
+    const postObject = {
+        "language": formLanguage.value,
+        "src": formImgUrl.value,
+    }
+    // Call post request to add a new article
+    fetch('http://localhost:3000/gists', {
+        method: 'post',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(postObject)
+    }).then(function () {
+        // Get the new article list
+        renderGists();
+
+        // Reset Form
+        resetForm();
+
+        // Close Modal
+        closeModal();
+    });
+}
+
+
+
+
+
+function getGist() {
+    let randomNumber = getRandomInt(18);
+
+    createAndRenderGist(data1[randomNumber]);
+
+    correctLanguage = data1[randomNumber].language;
+
+}
+
+function createAndRenderGist(gist) {
+    //   Create
+    const img = document.createElement('img');
+    img.setAttribute("src", gist.src);
+    img.setAttribute('id', 'gist');
+
+    //  Render
+    codeSnippet.innerText = '';
+    codeSnippet.appendChild(img);
+
+
+}
+
+function getAnswersButtons() {
+    
+    answers.innerText = '';
+    var idx = 0;
+
+    //   Create
+    const { randomAnswers, rightNumberIndex } = generateAnswers();
+
+    randomAnswers.forEach((answer, index) => {
+        const button = document.createElement('button');
+        button.setAttribute('class', 'buton_rasp');
+        button.setAttribute('class', 'animatedButton');
+        button.setAttribute('id', 'buton'+idx);
+        button.innerText = answer;
+        if (index === rightNumberIndex) {
+            button.addEventListener('click', function() {
+                //button.classList.add("buton_success");
+                button.classList.remove("animatedButton");
+                button.classList.add("apasatBun");
+                success();
+            })
+        } else {
+            button.addEventListener('click', function() {
+                button.classList.remove("animatedButton");
+                button.classList.add("apasatProst");
+                for (let i = 0; i < randomAnswers.length; i++){
+                    if(i === rightNumberIndex){
+                        const correctButton = document.getElementById('buton'+i);
+                        correctButton.classList.remove("animatedButton");
+                        correctButton.classList.add("apasatBun");
+                    }
+                }
+
+                
+                fail();
+            })
+        }
+        idx++;
+        answers.appendChild(button);
+
+    });
+    
+
+}
+
+function generateAnswers() {
+    let randomAnswers = [];
+
+    while (randomAnswers.length != 4) {
+        var randomAns = languages[getRandomInt(languages.length - 1)];
+        if (!randomAnswers.includes(randomAns) && randomAns != correctLanguage) {
+            randomAnswers.push(randomAns);
+        }
+    }
+
+    rightNumberIndex = getRandomInt(randomAnswers.length);
+
+    randomAnswers[rightNumberIndex] = correctLanguage;
+
+    return { randomAnswers, rightNumberIndex };
+
+}
+
+
+function refresh() {
+
+    getGist();
+
+    getAnswersButtons();
+
+    console.log(correctLanguage);
+
+}
+
+
+
+function success() {
+    points += 10;
+    document.getElementById("top_score_number_id").textContent = points;
+    const buttonNext = document.createElement('button');
+    buttonNext.innerText = 'Next';
+    //buttonNext.setAttribute('class', 'buton_next');
+    buttonNext.setAttribute('class', 'animatedButton');
+
+    next.appendChild(buttonNext);
+
+    buttonNext.addEventListener('click', function (){
+        next.removeChild(buttonNext);
+        refresh();
+        
+    });
+
+}
+
+function fail() {
+    const spanNumber = document.createElement('span');
+    spanNumber.textContent = points;
+    spanNumber.setAttribute('class', 'final_score_number');
+
+    const spanText = document.createElement('span');
+    spanText.textContent = ' Points';
+    spanText.setAttribute('class', 'final_score_text');
+
+    retry.appendChild(spanNumber);
+    retry.appendChild(spanText);
+
+    const finalText = document.createElement('p');
+    finalText.innerText = 'Better luck next time!';
+    finalText.setAttribute('class', 'end_text');
+
+    retry.appendChild(finalText);
+    
+    const buttonRetry = document.createElement('button');
+    buttonRetry.innerText = 'Retry';
+    buttonRetry.setAttribute('class', 'buton_retry');
+    buttonRetry.setAttribute('class', 'animatedButton');
+    
+    next.appendChild(buttonRetry);
+
+    buttonRetry.addEventListener('click', function(){
+        points = 0;
+        document.getElementById("top_score_number_id").textContent = points;
+        retry.removeChild(spanNumber);
+        retry.removeChild(spanText);
+        retry.removeChild(finalText);
+        next.removeChild(buttonRetry);
+        refresh();
+    })
+
+
+}
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+
+function resetForm() {
+    formLanguage.value = '';
+    formImgUrl.value = '';
+}
+
+function openAddModal() {
+
+    // clear all events save button events
+    clearSaveButtonEvents();
+
+    saveButton.addEventListener('click', function () {
+        addGistToServer();
+    });
+
+    body.className = 'show-modal';
+}
+
+function closeModal() {
+    body.className = '';
+}
+
+function clearSaveButtonEvents() {
+    let newUpdateButton = saveButton.cloneNode(true);
+    saveButton.parentNode.replaceChild(newUpdateButton, saveButton);
+    saveButton = document.getElementById('save');
+}
+
+cancelButton.addEventListener('click', closeModal);
+
+
+
+//refresh();
+
---
+++ index.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+var idx = 19;
+
+const fs = require("fs");
+
+// Aplicatia
+const app = express();
+
+// Middleware
+app.use(morgan("tiny"));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.use("/", express.static('frontend'));
+
+
+// Create
+app.post("/gists", (req, res) => {
+  const gistsList = readJSONFile();
+  const newGist = req.body;
+  newGist.id = idx;
+  const newGistList = [...gistsList, newGist];
+  writeJSONFile(newGistList);
+  res.json(newGist);
+  idx++;
+});
+
+// Read One
+app.get("/gists/:id", (req, res) => {
+  const gistsList = readJSONFile();
+  const id = req.params.id;
+  let idFound = false;
+  let foundGist;
+
+  gistsList.forEach(gist => {
+    if (id === gist.id) {
+      idFound = true;
+      foundGist = gist
+    }
+  });
+
+  if (idFound) {
+    res.json(foundGist);
+  } else {
+    res.status(404).send(`gist ${id} was not found`);
+  }
+});
+
+// Read All
+app.get("/gists", (req, res) => {
+  const gistsList = readJSONFile();
+  res.json(gistsList);
+});
+
+// Update
+app.put("/gists/:id", (req, res) => {
+  const gistsList = readJSONFile();
+  const id = req.params.id;
+  const newGist = req.body;
+  newGist.id = id;
+  idFound = false;
+
+  const newGistsList = gistsList.map((gist) => {
+     if (gist.id === id) {
+       idFound = true;
+       return newGist
+     }
+    return gist
+  })
+  
+  writeJSONFile(newGistsList);
+
+  if (idFound) {
+    res.json(newGist);
+  } else {
+    res.status(404).send(`gist ${id} was not found`);
+  }
+
+});
+
+// Delete
+app.delete("/gists/:id", (req, res) => {
+  const gistsList = readJSONFile();
+  const id = req.params.id;
+  const newGistsList = gistsList.filter((gist) => gist.id !== id)
+
+  if (gistsList.length !== newGistsList.length) {
+    res.status(200).send(`gist ${id} was removed`);
+    writeJSONFile(newGistsList);
+  } else {
+    res.status(404).send(`gist ${id} was not found`);
+  }
+});
+
+// Functia de citire din fisierul db.json
+function readJSONFile() {
+  return JSON.parse(fs.readFileSync("db.json"))["gists"];
+}
+
+// Functia de scriere in fisierul db.json
+function writeJSONFile(content) {
+  fs.writeFileSync(
+    "db.json",
+    JSON.stringify({ gists: content }),
+    "utf8",
+    err => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+}
+
+// Pornim server-ul
+app.listen("3000", () =>
+  console.log("Server started at: http://localhost:3000")
+);
